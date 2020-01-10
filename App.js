300267e4b9b310947a2300042295365cca88e98c
anhtu0504 <44833951+anhtu0504@users.noreply.github.com>
2020-01-10 10:30:38 +0700

    add stylesheet

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CategoryListItem from './components/CategoryListItem';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+          <CategoryListItem props />
+          <CategoryListItem />
+          <CategoryListItem />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
-  },
+    paddingLeft: 16,
+    paddingRight: 16
+  }
 });
+     
