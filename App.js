0e91fe47eeaea7e39671d582a35fb22a3d6f46a8
anhtu0504 <44833951+anhtu0504@users.noreply.github.com>
2020-01-13 11:08:42 +0700

    add FlatList

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, View, FlatList } from 'react-native';
 import CategoryListItem from './components/CategoryListItem';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { id: 1,name: 'Dụng cụ trượt tuyết'},
+        { id: 2,name: 'Quần áo trượt tuyết'},
+        { id: 3,name: 'Kính mũ'}
+      ]
+    };
+  }       
+
   render() {
+    const { categories } = this.state;
     return (
-      <View>
-          <ScrollView style={{ paddingLeft: 16, paddingRight: 16}} contentContainerStyle={{backgroundColor: '#F00'}}>
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          </ScrollView>
-      </View>
-    );
+        <FlatList data={categories} 
+          renderItem={({ item }) => <CategoryListItem  category={item} />}
+          keyExtractor={item => `${item.id}`}
+          contentContainerStyle={{ paddingLeft: 16, paddingRight: 16 }} 
+        />
+    );  
   }
 }
 
